21a830d8b221049e85eb6df6f81a7bc2328e951e
Akeem Philbert <akeem.philbert@wepala.com>
2019-12-14 19:48:23 -0400

    WEOS-79 Add the ability to specify a priority on a handler
    * Added a property "priority" to the middleware config
    * Added a sorter to sort the middleware configs

--- service/service_test.go
+++ service/service_test.go
@@ -4,6 +4,7 @@ import (
 	"bitbucket.org/wepala/weos-controller/service"
 	"encoding/json"
 	"net/http"
+	"net/http/httptest"
 	"runtime"
 	"strings"
 	"testing"
@@ -81,7 +82,7 @@ func TestNewControllerService(t *testing.T) {
 
 func TestControllerService_GetHandlers(t *testing.T) {
 	apiYaml := "testdata/api/basic-site-api." + runtime.GOOS + ".yml"
-	handlerNames := make([]string, 1)
+	var handlerNames []string
 	config := Config{}
 	//setup mock
 	weosPluginMock := &PluginInterfaceMock{
@@ -114,6 +115,9 @@ func TestControllerService_GetHandlers(t *testing.T) {
 	}
 
 	s, err := service.NewControllerService(apiYaml, pluginLoaderMock)
+	if err != nil {
+		t.Fatalf("got an error while create new controller service %s", err)
+	}
 
 	//get path config
 	pathConfig, err := s.GetPathConfig("/about", "get")
@@ -143,4 +147,94 @@ func TestControllerService_GetHandlers(t *testing.T) {
 		t.Errorf("expected add handler config to be called %d time, called %d times", 1, len(weosPluginMock.AddPathConfigCalls()))
 	}
 
+}
+
+func TestControllerService_HandlerPriority(t *testing.T) {
+	apiYaml := "testdata/api/basic-site-api." + runtime.GOOS + ".yml"
+	var handlerNames []string
+	config := Config{}
+	//setup mock
+	weosPluginMock1 := &PluginInterfaceMock{
+		GetHandlerByNameFunc: func(name string) http.HandlerFunc {
+			return func(writer http.ResponseWriter, request *http.Request) {
+				handlerNames = append(handlerNames, name)
+			}
+		},
+		AddConfigFunc: func(tconfig json.RawMessage) error {
+			//check the config on the middleware
+			tbytes, err := tconfig.MarshalJSON()
+			if err != nil {
+				t.Fatalf("encountered error marshaling json for config")
+			}
+			if err = json.Unmarshal(tbytes, &config); err != nil {
+				t.Fatalf("encountered error unmarshaling json for config")
+			}
+
+			return nil
+		},
+		AddPathConfigFunc: func(handler string, config json.RawMessage) error {
+			return nil
+		},
+	}
+
+	weosPluginMock2 := &PluginInterfaceMock{
+		GetHandlerByNameFunc: func(name string) http.HandlerFunc {
+			return func(writer http.ResponseWriter, request *http.Request) {
+				handlerNames = append(handlerNames, name)
+			}
+		},
+		AddConfigFunc: func(tconfig json.RawMessage) error {
+			//check the config on the middleware
+			tbytes, err := tconfig.MarshalJSON()
+			if err != nil {
+				t.Fatalf("encountered error marshaling json for config")
+			}
+			if err = json.Unmarshal(tbytes, &config); err != nil {
+				t.Fatalf("encountered error unmarshaling json for config")
+			}
+
+			return nil
+		},
+		AddPathConfigFunc: func(handler string, config json.RawMessage) error {
+			return nil
+		},
+	}
+
+	pluginLoaderMock := &PluginLoaderInterfaceMock{
+		GetPluginFunc: func(fileName string) (pluginInterface service.PluginInterface, e error) {
+			if strings.Contains(fileName, "testdata/plugins/test2") {
+				return weosPluginMock2, nil
+			}
+			return weosPluginMock1, nil
+		},
+	}
+
+	s, err := service.NewControllerService(apiYaml, pluginLoaderMock)
+	if err != nil {
+		t.Fatalf("got an error while create new controller service %s", err)
+	}
+
+	//get path config
+	pathConfig, err := s.GetPathConfig("/multiple-handlers", "get")
+	if err != nil {
+		t.Fatalf("issue getting path config: '%v", err)
+	}
+
+	//use path config to get handlers
+	handlers, _ := s.GetHandlers(pathConfig)
+	if len(handlers) != 2 {
+		t.Errorf("expected %d handlers to be loaded: got %d [%s]", 2, len(handlers), strings.Join(handlerNames, ","))
+	}
+
+	rw := httptest.NewRecorder()
+	r := httptest.NewRequest("get", "/foo", nil)
+	handlers[0].ServeHTTP(rw, r)
+
+	if len(handlerNames) != 1 {
+		t.Fatalf("handlers were not called")
+	}
+
+	if handlerNames[0] != "FooBar" {
+		t.Errorf("expected the first handler to be %s, got %s", "FooBar", handlerNames[0])
+	}
 }
