a45cbb64c1e788438c39f19af8870858dbfeeb94
Marcus Sanatan <marcus.sanatan@wepala.com>
2020-04-07 13:22:43 -0400

    WEOS-320 Allow for * wildcard in routes
    This allows multiple routes to be serviced by one path.
    This will make forwarding to Oathkeeper a lot simpler.
    Oathkeeper can handle *, so it makes the reverse proxy easier

--- service/httpapi_test.go
+++ service/httpapi_test.go
@@ -1,20 +1,22 @@
 package service_test
 
 import (
-	"bitbucket.org/wepala/weos-controller/service"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/getkin/kin-openapi/openapi3"
-	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 	"testing"
+
+	"bitbucket.org/wepala/weos-controller/service"
+	"github.com/getkin/kin-openapi/openapi3"
+	log "github.com/sirupsen/logrus"
 )
 
 //go:generate moq -out testing_mocks_test.go -pkg service_test . ServiceInterface
@@ -43,11 +45,11 @@ var mockServerTests = []*HTTPTest{
 }*/
 
 var httpServerTests = []*HTTPTest{
-	//{
-	//	name:        "about_page_200",
-	//	testDataDir: "testdata/html/http",
-	//	apiFixture:  "testdata/api/basic-site-api." + runtime.GOOS + ".yml",
-	//},
+	{
+		name:        "about_page_200",
+		testDataDir: "testdata/html/http",
+		apiFixture:  "testdata/api/basic-site-api." + runtime.GOOS + ".yml",
+	},
 	{
 		name:        "x_mock_status_code",
 		testDataDir: "testdata/html/http",
@@ -67,6 +69,21 @@ var httpServerTests = []*HTTPTest{
 		name:        "x_mock_array_component_example",
 		testDataDir: "testdata/html/http",
 		apiFixture:  "testdata/api/x-mock-status-code.yaml",
+	},
+	{
+		name:        "wildcard",
+		testDataDir: "testdata/html/http",
+		apiFixture:  "testdata/api/http-test-api-wildcard.yaml",
+	},
+	{
+		name:        "wildcard_nested",
+		testDataDir: "testdata/html/http",
+		apiFixture:  "testdata/api/http-test-api-wildcard.yaml",
+	},
+	{
+		name:        "wildcard_root",
+		testDataDir: "testdata/html/http",
+		apiFixture:  "testdata/api/http-test-api-wildcard.yaml",
 	},
 }
 
@@ -538,14 +555,14 @@ func TestMockHandler_ServeHTTPErrors(t *testing.T) {
 
 }
 
-func TestOtherSwaggerFiles(t *testing.T)  {
+func TestOtherSwaggerFiles(t *testing.T) {
 	t.Run("test callback example", func(t *testing.T) {
 		loader := openapi3.NewSwaggerLoader()
 		config, err := loader.LoadSwaggerFromFile("testdata/api/callback-example.yaml")
 
 		if err != nil {
 			t.Fatalf("error loading %s: %s", "testdata/api/callback-example.yaml", err.Error())
-	}
+		}
 
 		log.Debugf("Load input fixture: %s", "x_mock_no_status_code.input.http")
 		request := loadHttpRequestFixture(filepath.Join("testdata/html/http", "x_mock_no_status_code.input.http"), t)
@@ -561,7 +578,7 @@ func TestOtherSwaggerFiles(t *testing.T)  {
 
 		log.Info(string(body))
 
-		if string(body) == ""{
+		if string(body) == "" {
 			t.Error("Expected something to be returned")
 		}
 	})
@@ -588,7 +605,7 @@ func TestOtherSwaggerFiles(t *testing.T)  {
 
 		log.Info(string(body))
 
-		if string(body) == ""{
+		if string(body) == "" {
 			t.Error("Expected something to be returned")
 		}
 	})
@@ -615,7 +632,7 @@ func TestOtherSwaggerFiles(t *testing.T)  {
 
 		log.Info(string(body))
 
-		if string(body) == ""{
+		if string(body) == "" {
 			t.Error("Expected something to be returned")
 		}
 	})
@@ -642,7 +659,7 @@ func TestOtherSwaggerFiles(t *testing.T)  {
 
 		log.Info(string(body))
 
-		if string(body) == ""{
+		if string(body) == "" {
 			t.Error("Expected something to be returned")
 		}
 	})
@@ -669,7 +686,7 @@ func TestOtherSwaggerFiles(t *testing.T)  {
 
 		log.Info(string(body))
 
-		if string(body) == ""{
+		if string(body) == "" {
 			t.Error("Expected something to be returned")
 		}
 	})
@@ -696,7 +713,7 @@ func TestOtherSwaggerFiles(t *testing.T)  {
 
 		log.Info(string(body))
 
-		if string(body) == ""{
+		if string(body) == "" {
 			t.Error("Expected something to be returned")
 		}
 	})
