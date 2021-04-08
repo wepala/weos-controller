321d002f7c16c0edc88fee57c5c6c1087c6e9030
Akeem Philbert <akeem.philbert@wepala.com>
2021-04-08 07:21:29 -0400

    Add test to ensure record request middleware works
    * Updated record request middleware to use DumpRequestOut to generate representative requests

--- go.mod
+++ go.mod
@@ -3,15 +3,14 @@ module github.com/wepala/weos-controller
 go 1.13
 
 require (
+
 	github.com/getkin/kin-openapi v0.15.0
 	github.com/labstack/echo/v4 v4.2.1
 	github.com/lib/pq v1.8.0 // indirect
 	github.com/onsi/ginkgo v1.15.2 // indirect
 	github.com/onsi/gomega v1.11.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/segmentio/ksuid v1.0.3
 	github.com/sirupsen/logrus v1.7.0
-	github.com/spf13/viper v1.7.1 // indirect
 	github.com/wepala/weos v0.0.7-0.20210405143825-1c327233f75b
 	github.com/ziutek/mymysql v1.5.4 // indirect
 )
---
+++ weos_api_test.go
@@ -0,0 +1,77 @@
+package weoscontroller_test
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"github.com/labstack/echo/v4"
+	weoscontroller "github.com/wepala/weos-controller"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestAPI_RequestRecording(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPut, "http://example.com/endpoint", strings.NewReader(`{"name":"Sojourner Truth","email":"sojourner@examle.com"}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	api := &weoscontroller.API{Config: &weoscontroller.APIConfig{
+		RecordingBaseFolder: ".",
+	}}
+	e.PUT("/endpoint", func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}, api.RequestRecording)
+	api.SetEchoInstance(e)
+	e.ServeHTTP(rec, req)
+	//confirm file is created
+	if _, err := os.Stat("./_endpoint.input.http"); os.IsNotExist(err) {
+		t.Fatalf("expected fixture file to be created")
+	}
+
+	//confirm contents of file
+	request := loadHttpRequestFixture("./_endpoint.input.http", t)
+	var bodyStruct struct {
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	}
+	err := json.NewDecoder(request.Body).Decode(&bodyStruct)
+	if err != nil {
+		t.Fatalf("error unmarshalling request '%s'", err)
+	}
+
+	if bodyStruct.Name != "Sojourner Truth" {
+		t.Errorf("expected the name to be '%s', got '%s'", "Sojourner Truth", bodyStruct.Name)
+	}
+
+	//delete file
+	err = os.Remove("./_endpoint.input.http")
+	if err != nil {
+		t.Fatalf("unable to delete test fixture created '%s', got error '%s'", "./_endpoint.input.http", err)
+	}
+}
+
+//loadHttpRequestFixture wrapper around the test helper to make it easier to use it with test table
+func loadHttpRequestFixture(filename string, t *testing.T) *http.Request {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatalf("test fixture '%s' not loaded %v", filename, err)
+	}
+
+	reader := bufio.NewReader(bytes.NewReader(data))
+	request, err := http.ReadRequest(reader)
+	if err == io.EOF {
+		return request
+	}
+
+	if err != nil {
+		t.Fatalf("test fixture '%s' not loaded %v", filename, err)
+	}
+
+	return request
+}
