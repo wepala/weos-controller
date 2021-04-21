1e6fc14a9fd6f09972ce378510edef729a1442be
Atonia Andall <atonia.andall@wepala.com>
2021-04-21 06:50:15 -0400

    feature/WEOS-919 Added expired token case

--- weos_api_test.go
+++ weos_api_test.go
@@ -214,6 +214,56 @@ func TestAPI_Authenticate(t *testing.T) {
 			t.Errorf("expected the response message to be %s got %s", `{"message":"missing or malformed jwt"}`, responseMessage)
 		}
 	})
+	t.Run("jwt authenticate expired token", func(t *testing.T) {
+		// Setup
+		e := echo.New()
+		key := "secureSecretText"
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{ExpiresAt: jwt.TimeFunc().Unix() - 100})
+		signedToken, err := token.SignedString([]byte(key))
+		var bearer = "Bearer " + signedToken
+		if err != nil {
+			t.Errorf("got an error setting up tests %s", err)
+		}
+		req := httptest.NewRequest(http.MethodPost, "/endpoint", strings.NewReader(`{"name":"Sojourner Truth","email":"sojourner@examle.com"}`))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		req.Header.Set("Authorization", bearer)
+		rec := httptest.NewRecorder()
+		api := &weoscontroller.API{Config: &weoscontroller.APIConfig{
+			RecordingBaseFolder: ".",
+			JWTConfig: &weoscontroller.JWTConfig{
+				Key:             key,
+				SigningKeys:     map[string]interface{}{},
+				Certificate:     nil,
+				CertificatePath: "",
+				TokenLookup:     "",
+				AuthScheme:      "",
+				SigningMethod:   "HS256",
+				ContextKey:      "",
+			},
+		}}
+
+		e.POST("/endpoint", func(c echo.Context) error {
+			return c.String(http.StatusOK, "test")
+		}, api.Authenticate)
+
+		api.SetEchoInstance(e)
+		e.ServeHTTP(rec, req)
+
+		response := rec.Result()
+		defer response.Body.Close()
+
+		if response.StatusCode != 401 {
+			t.Errorf("expected the status code to be %d, got %d", 401, response.StatusCode)
+		}
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			t.Errorf("got an error getting response body %s", err)
+		}
+		responseMessage := strings.TrimSpace(string(bodyBytes))
+		if responseMessage != `{"message":"invalid or expired jwt"}` {
+			t.Errorf("expected the response message to be %s got %s", `{"message":"invalid or expired jwt"}`, responseMessage)
+		}
+	})
 	t.Run("jwt authenticate invalid token", func(t *testing.T) {
 		// Setup
 		e := echo.New()
@@ -254,6 +304,14 @@ func TestAPI_Authenticate(t *testing.T) {
 
 		if response.StatusCode != 401 {
 			t.Errorf("expected the status code to be %d, got %d", 401, response.StatusCode)
+		}
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			t.Errorf("got an error getting response body %s", err)
+		}
+		responseMessage := strings.TrimSpace(string(bodyBytes))
+		if responseMessage != `{"message":"invalid or expired jwt"}` {
+			t.Errorf("expected the response message to be %s got %s", `{"message":"invalid or expired jwt"}`, responseMessage)
 		}
 	})
 }
