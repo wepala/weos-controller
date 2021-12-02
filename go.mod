71d6b33b471ae2e8e212c99e2667e205c2602d9b
shaniah868 <shaniahworld@hotmail.com>
2021-12-02 09:12:06 -0400

    feature/36 configuring grpc middleware -Made a new package to hold grpc files -Added intialize function

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/getkin/kin-openapi v0.15.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
 	github.com/labstack/echo/v4 v4.5.0
 	github.com/labstack/gommon v0.3.0
 	github.com/lestrrat-go/jwx v1.2.1
---
+++ weosgrpc/main.go
@@ -0,0 +1,98 @@
+package weosgrpc
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strings"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/labstack/echo"
+	weoscontroller "github.com/wepala/weos-controller"
+)
+
+func InitalizeGrpc(ctx *context.Context, api weoscontroller.APIInterface, apiConfig string) *context.Context {
+	var content []byte
+	var err error
+	//try load file if it's a yaml file otherwise it's the contents of a yaml file WEOS-1009
+	if strings.Contains(apiConfig, ".yaml") || strings.Contains(apiConfig, "/yml") {
+		content, err = ioutil.ReadFile(apiConfig)
+		if err != nil {
+			//e.Logger.Fatalf("error loading api specification '%s'", err)
+		}
+	} else {
+		content = []byte(apiConfig)
+	}
+
+	//change the $ref to another marker so that it doesn't get considered an environment variable WECON-1
+	tempFile := strings.ReplaceAll(string(content), "$ref", "__ref__")
+	//replace environment variables in file
+	tempFile = os.ExpandEnv(string(tempFile))
+	tempFile = strings.ReplaceAll(string(tempFile), "__ref__", "$ref")
+	content = []byte(tempFile)
+	loader := openapi3.NewSwaggerLoader()
+	swagger, err := loader.LoadSwaggerFromData(content)
+	if err != nil {
+		//e.Logger.Fatalf("error loading api specification '%s'", err)
+	}
+
+	//parse the main config
+	var config *weoscontroller.APIConfig
+	if swagger.ExtensionProps.Extensions["x-weos-config"] != nil {
+
+		data, err := swagger.ExtensionProps.Extensions["x-weos-config"].(json.RawMessage).MarshalJSON()
+		if err != nil {
+			//e.Logger.Fatalf("error loading api config '%s", err)
+			return ctx
+		}
+		err = json.Unmarshal(data, &config)
+		if err != nil {
+			//e.Logger.Fatalf("error loading api config '%s", err)
+			return ctx
+		}
+
+		err = api.AddConfig(config)
+		if err != nil {
+			//e.Logger.Fatalf("error setting up module '%s", err)
+			return ctx
+		}
+		//setup global pre middleware
+		var preMiddlewares []echo.MiddlewareFunc
+		for _, middlewareName := range config.PreMiddleware {
+			t := reflect.ValueOf(api)
+			m := t.MethodByName(middlewareName)
+			if !m.IsValid() {
+				//e.Logger.Fatalf("invalid handler set '%s'", middlewareName)
+			}
+			preMiddlewares = append(preMiddlewares, m.Interface().(func(handlerFunc echo.HandlerFunc) echo.HandlerFunc))
+		}
+
+		//setup global middleware
+		var middlewares []echo.MiddlewareFunc
+		//prepend Context middleware
+		config.Middleware = append([]string{"Context"}, config.Middleware...)
+		for _, middlewareName := range config.Middleware {
+			if middlewareName == "Context" {
+				t := reflect.ValueOf(api)
+				m := t.MethodByName(middlewareName)
+				middlewares = append(middlewares, m.Interface().(func(handlerFunc echo.HandlerFunc) echo.HandlerFunc))
+				continue
+			}
+			t := reflect.ValueOf(api)
+			m := t.MethodByName(middlewareName)
+			if !m.IsValid() {
+				//e.Logger.Fatalf("invalid handler set '%s'", middlewareName)
+			}
+			middlewares = append(middlewares, m.Interface().(func(handlerFunc echo.HandlerFunc) echo.HandlerFunc))
+		}
+
+		err = api.Initialize()
+		if err != nil {
+			//e.Logger.Fatalf("error initializing application '%s'", err)
+			return ctx
+		}
+	}
+	return ctx
+}
